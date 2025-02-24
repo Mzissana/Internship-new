35c9cb535b16ac965736a8e94dc45918c5c168ae
Mzissana <kdk459915@hotmail.com>
2025-02-24 22:58:57 +0300

    Добавляет верстку

---
+++ js/main.js
@@ -0,0 +1,23 @@
+import initProgramsSwiper from './scripts/initialize-programs-swiper.js';
+import initReviewsSwiper from './scripts/initialize-reviews-swiper.js';
+import initNewsSwiper from './scripts/initialize-news-swiper.js';
+import initHeroSwiper from './scripts/initialize-hero-swiper.js';
+import activateMenu from './scripts/activate-menu.js';
+import activateFAQAccordion from './scripts/activate-accordion.js';
+import activateModal from './scripts/activate-modal.js';
+import initFormValidation from './scripts/validate-form.js';
+import initModalFormValidation from './scripts/validate-modal-form.js';
+import initCustomDropdown from './scripts/initialize-custom-dropdown.js';
+import initModalDropdown from './scripts/initialize-modal-dropdown.js';
+
+document.addEventListener('DOMContentLoaded', initProgramsSwiper);
+document.addEventListener('DOMContentLoaded', initReviewsSwiper);
+document.addEventListener('DOMContentLoaded', initNewsSwiper);
+document.addEventListener('DOMContentLoaded', initHeroSwiper);
+document.addEventListener('DOMContentLoaded', activateMenu);
+document.addEventListener('DOMContentLoaded', activateFAQAccordion);
+document.addEventListener('DOMContentLoaded', activateModal);
+document.addEventListener('DOMContentLoaded', initFormValidation);
+document.addEventListener('DOMContentLoaded', initModalFormValidation);
+document.addEventListener('DOMContentLoaded', initCustomDropdown);
+document.addEventListener('DOMContentLoaded', initModalDropdown);
